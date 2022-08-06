84c773c47105e97c857d34889c81824dac7e4093
Gino Luiggi Chirinos Plasencia <lchirinos@acercapartners.com>
2022-08-06 21:21:10 +0200

    First commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/quotes */
+/* eslint-disable arrow-body-style */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AnimationController, IonButton, IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +11,64 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  @ViewChild(IonModal, { static: false }) modal: IonModal;
+  @ViewChildren(IonButton, { read: ElementRef }) buttons: QueryList<ElementRef>;
+
+  selectedButton: HTMLElement;
+
+  constructor(private animationCtrl: AnimationController) {}
+
+  enterAnimation = (baseEl: HTMLElement) => {
+    const root = baseEl.shadowRoot;
+
+    // const first = document.querySelector('ion-button').getBoundingClientRect();
+    const first = this.selectedButton.getBoundingClientRect();
+    const last = document.querySelector('ion-app').getBoundingClientRect();
+
+    // Invert
+    const invert = {
+      x: first.left - last.left,
+      y: first.top - last.top,
+      scaleX: first.width / last.width,
+      scaleY: first.height / last.height,
+    };
+
+    const backdropAnimation = this.animationCtrl
+      .create()
+      .addElement(root.querySelector('ion-backdrop')!)
+      .fromTo('opacity', '0.01', 'var(--backdrop-opacity)');
+
+    const wrapperAnimation = this.animationCtrl
+      .create()
+      .addElement(root.querySelector('.modal-wrapper')!)
+      .beforeStyles({
+        ["transform-origin"]: "0 0",
+      })
+      .fromTo(
+        "transform",
+        `translate(${invert.x}px, ${invert.y}px) scale(${invert.scaleX}, ${invert.scaleY})`,
+        "translate(0, 0) scale(1, 1)"
+      )
+      .fromTo("opacity", "0", "1");
+
+    return this.animationCtrl
+      .create()
+      .addElement(baseEl)
+      .easing('ease-out')
+      .duration(400)
+      .addAnimation([backdropAnimation, wrapperAnimation]);
+  };
+
+  leaveAnimation = (baseEl: HTMLElement) => {
+    return this.enterAnimation(baseEl).direction('reverse');
+  };
+
+  open(index: number): void {
+    const button = this.buttons.get(index).nativeElement;
+
+    this.selectedButton = button;
+
+    this.modal.present();
+  }
 
 }
